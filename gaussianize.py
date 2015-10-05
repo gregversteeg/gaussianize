3acfbe2b921cec192913f14aef7ecd05974ca9d2
Greg Ver Steeg <gversteeg@gmail.com>
2015-10-05 10:58:25 -0700

    Added boxcox and brute force methods to gaussianize data.

--- gaussianize.py
+++ gaussianize.py
@@ -1,25 +1,20 @@
 """
-Transform samples of heavy-tailed data so that it is approximately normally distributed
-
-Based on the paper:
-The Lambert Way to Gaussianize heavy tailed data with the inverse of Tukey's h transformation as a special case
-Based on algorithm in Appendix C
-Author generously provides code in R:
-https://cran.r-project.org/web/packages/LambertW/
+Transform data so that it is approximately normally distributed
 
 This code written by Greg Ver Steeg, 2015.
 """
 
 import numpy as np
 from scipy.special import lambertw
-from scipy.stats import kurtosis
+from scipy.stats import kurtosis, norm, rankdata, boxcox
 from scipy.optimize import fmin  # TODO: Explore efficacy of other opt. methods
 
 np.seterr(all='warn')
 
-class Lambert(object):
+
+class Gaussianize(object):
     """
-    Gaussianize heavy-tailed data using Lambert's W.
+    Gaussianize data using various methods.
 
     Conventions
     ----------
@@ -34,21 +29,29 @@ class Lambert(object):
     max_iter : int, default = 200
         Maximum number of iterations to search for correct parameters of Lambert transform.
 
+    strategy : str, default='lambert'
+        Possibilities are 'lambert'[1], 'brute'[2] and 'boxcox'[3].
+
     Attributes
     ----------
     taus : list of tuples
-        For each variable, we have a tuple consisting of (mu, sigma, delta), corresponding to the parameters of the
+        For each variable, we have transformation parameters.
+        For Lambert, e.g., a tuple consisting of (mu, sigma, delta), corresponding to the parameters of the
         appropriate Lambert transform. Eq. 6 and 8 in the paper below.
 
     References
     ----------
     [1] Georg Goerg. The Lambert Way to Gaussianize heavy tailed data with
                         the inverse of Tukey's h transformation as a special case
+        Author generously provides code in R: https://cran.r-project.org/web/packages/LambertW/
+    [2] Valero Laparra, Gustavo Camps-Valls, and Jesus Malo. Iterative Gaussianization: From ICA to Random Rotations
+    [3] Box cox transformation and references: https://en.wikipedia.org/wiki/Power_transform
     """
 
-    def __init__(self, tol=1.22e-4, max_iter=100):
+    def __init__(self, tol=1.22e-4, max_iter=100, strategy='lambert'):
         self.tol = tol
         self.max_iter = max_iter
+        self.strategy = strategy
         self.taus = []  # Store tau for each transformed variable
 
     def fit(self, x):
@@ -57,8 +60,19 @@ class Lambert(object):
             x = x[:, np.newaxis]
         elif len(x.shape) != 2:
             print "Data should be a 1-d list of samples to transform or a 2d array with samples as rows."
-        for x_i in x.T:
-            self.taus.append(igmm(x_i, tol=self.tol, max_iter=self.max_iter))
+
+        if self.strategy == 'lambert':
+            for x_i in x.T:
+                self.taus.append(igmm(x_i, tol=self.tol, max_iter=self.max_iter))
+        elif self.strategy == 'brute':
+            for x_i in x.T:
+                self.taus.append(None)  # TODO: In principle, we could store parameters to do a quasi-invert
+        elif self.strategy == 'boxcox':
+            for x_i in x.T:
+                self.taus.append(boxcox(x_i)[1])
+        else:
+            raise NotImplementedError
+
 
     def transform(self, x):
         x = np.asarray(x)
@@ -69,14 +83,27 @@ class Lambert(object):
         if x.shape[1] != len(self.taus):
             print "%d variables in test data, but %d variables were in training data." % (x.shape[1], len(self.taus))
 
-        return np.array([w_t(x_i, tau_i) for x_i, tau_i in zip(x.T, self.taus)]).T
+        if self.strategy == 'lambert':
+            return np.array([w_t(x_i, tau_i) for x_i, tau_i in zip(x.T, self.taus)]).T
+        elif self.strategy == 'brute':
+            return np.array([norm.ppf((rankdata(x_i) - 0.5) / len(x_i)) for x_i in x.T]).T
+        elif self.strategy == 'boxcox':
+            return np.array([boxcox(x_i, lmbda=lmbda_i) for x_i, lmbda_i in zip(x.T, self.taus)]).T
+        else:
+            raise NotImplementedError
 
     def fit_transform(self, x):
         self.fit(x)
         return self.transform(x)
 
     def invert(self, y):
-        return np.array([inverse(y_i, tau_i) for y_i, tau_i in zip(y.T, self.taus)]).T
+        if self.strategy == 'lambert':
+            return np.array([inverse(y_i, tau_i) for y_i, tau_i in zip(y.T, self.taus)]).T
+        elif self.strategy == 'boxcox':
+            return np.array([(1. + lmbda_i * y_i)**(1./lmbda_i) for y_i, lmbda_i in zip(y.T, self.taus)]).T
+        else:
+            print 'Inversion not supported for this gaussianization transform.'
+            raise NotImplementedError
 
     def qqplot(self, x, prefix='qq'):
         """Show qq plots compared to normal before and after the transform."""
@@ -187,6 +214,12 @@ if __name__ == '__main__':
                      help="Separator between entries in the data, default is ','.")
     parser.add_option_group(group)
 
+    group = OptionGroup(parser, "Transform Options")
+    group.add_option("-s", "--strategy",
+                     action="store", dest="strategy", type="string", default="lambert",
+                     help="Strategy.")
+    parser.add_option_group(group)
+
     group = OptionGroup(parser, "Output Options")
     group.add_option("-o", "--output",
                      action="store", dest="output", type="string", default="gaussian_output.csv",
@@ -227,7 +260,7 @@ if __name__ == '__main__':
         traceback.print_exc(file=sys.stdout)
         sys.exit()
 
-    out = Lambert()
+    out = Gaussianize(strategy=options.strategy)
     y = out.fit_transform(X)
     with open(options.output, 'w') as csvfile:
         writer = csv.writer(csvfile, delimiter=options.delimiter)
--- readme.md
+++ readme.md
@@ -1,7 +1,25 @@
-# The Lambert Way to Gaussianize heavy-tailed data
+# Gaussianize data
 
-The idea is to apply a smooth, invertible transformation of some univariate data so that the distribution of the
- transformed data is as Gaussian as possible. For heavy-tailed data, there is a one-parameter family that based on
+
+The idea is to apply a smooth, invertible transformation to some univariate data so that the distribution of the
+ transformed data is as Gaussian as possible. This would be a pre-processing step for some other analysis. Why might
+ this be useful?
+ 
+ 1. A standard pre-processing step is to "whiten" data by subtracting the mean and scaling it to have standard deviation 1. 
+ Gaussianized data has these properties and 
+ 2. Robust statistics / reduce effect of outliers. Lots of real world data exhibits long tails. For machine learning, the small
+ number of examples in the tails of the distribution can have a large effect on results. 
+ Gaussianized data will "squeeze" the tails in towards the center. 
+ 3. Theoretical benefits. Gaussian distributions are very well studied with many unique properties that you may like to leverage. 
+ 4. Various information theoretic quantities are invariant under invertible transforms, so it makes sense to first 
+ transform into a friendlier distribution. 
+ 
+ 
+# Three ways to Gaussianize
+
+## The Lambert Way to Gaussianize heavy-tailed data
+
+For heavy-tailed data, there is a one-parameter family that based on
  Lambert's W function that does this very well. 
 This approach is described in Sec. C of the following paper. 
 > [The Lambert Way to Gaussianize heavy tailed data with the inverse of Tukey's h transformation as a special case](http://arxiv.org/abs/1010.2265)
@@ -20,7 +38,25 @@ After applying the transformation, this plot looks like this.
 
 ![Cauchy after](https://github.com/gregversteeg/gaussianize/blob/master/tests/cauchy_after.png?raw=true "Cauchy after")
 
-## Example usage
+## Box Cox transformation
+This is an old and well known way to [gaussianize data](https://en.wikipedia.org/wiki/Power_transform). 
+Some of the limitations of this approach are discussed in Georg's paper, above.  E.g., it only applies to positive data
+and gaussianizes data with a heavy right-hand tail. 
+
+This transformation is not yet implemented.
+
+## Brute force 
+
+The brute force method I first saw in [this paper](http://www.uv.es/~gcamps/papers/Laparra11.pdf), but I assume it
+has been described elsewhere before. The idea is to first make the distribution uniform by rank transforming the data. 
+Then you apply the inverse of the CDF for a normal. This results in very gaussian looking empirical data, but it has some
+drawbacks. 
+
+One drawback of this approach is that the first step, the empirical copula transform, is not a smooth transformation. 
+Furthermore, inverting this transformation can only be done in a kind of piecemeal approximate way which is not yet 
+implemented. 
+
+# Example usage
 
 ```python
 # This example is in tests. 
@@ -28,7 +64,7 @@ import gaussianize as g
 import numpy as np
 
 x = np.hstack([np.random.standard_cauchy(size=(1000, 2)), np.random.normal(size=(1000, 2))])
-out = g.Lambert()
+out = g.Gaussianize()
 out.fit(x)  # Learn the parameters for the transformation
 y = out.transform(x)  # Transform x to y, where y should be normal
 x_prime = out.invert(y)  # Inverting this transform should recover the data
@@ -37,7 +73,7 @@ out.qqplot(x)  # Plot qq plots for each variable, before and after.
 
 ```
 
-## Command line interface
+# Command line interface
 
 Preprocess a data file by Gaussianizing each column. The -q option optionally generates qq plots. Default delimiter is 
 comma. The default assumption is that the first row and column are labels. This can be altered with options. 
@@ -45,4 +81,5 @@ comma. The default assumption is that the first row and column are labels. This 
 ```
 python gaussianize.py file.csv --delimiter=',' -o output.csv -q
 python gaussianize.py -h  # For a list of all options
+python gaussianize.py file.csv --delimiter=',' -o output_brute.csv --strategy='brute' -q
 ```
